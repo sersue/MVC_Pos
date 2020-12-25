f92ac49237607af5f899ecf40cff0547f4deff69
MoonSueLym <msr56501354@gmail.com>
2020-12-25 13:25:06 +0900

    add graphic ui

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
+import jdk.jfr.internal.Repository;
+
 public class Main {
 
     private static CmdUI ui;
     // private static GraphicUI ui;
     public static void main(String[] args){
+
         
-        Controller controller = new Controller();
+        Controller controller = new Controller(repository);
         ui = new CmdUI(controller);
+        // ui = new GraphicUI(controller);
         ui.start();
     }
 }
