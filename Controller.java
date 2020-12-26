41e19ede97bca7664b448c51bd23af1cea9050dc
MoonSueLym <msr56501354@gmail.com>
2020-12-26 15:13:27 +0900

    add cmd ui

--- Controller.java
+++ Controller.java
@@ -1,3 +1,13 @@
 public class Controller {
-    
+
+
+    // public void makeNewSale(){
+
+    // }
+    // public void enterItem(itemid,quantity){
+
+    // }
+    // public void endSale(){
+
+    // }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
-import jdk.jfr.internal.Repository;
+import java.lang.ModuleLayer.Controller;
+import java.sql.*;
 
 public class Main {
 
     private static CmdUI ui;
     // private static GraphicUI ui;
     public static void main(String[] args){
+        Connection con;
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/?user=root", "root", "anstnfla25");
+            System.out.println("데이터베이스 접속에 성공했습니다");
 
+        }catch(SQLException ex){
+            System.out.println("SQLException"+ex);
+        }
+        catch(Exception ex){
+            System.out.println("Exception"+ex);
+        }
         
-        Controller controller = new Controller(repository);
-        ui = new CmdUI(controller);
-        // ui = new GraphicUI(controller);
-        ui.start();
+        // Controller controller = new Controller(con);
+
+        // ui = new CmdUI(controller);
+        // // ui = new GraphicUI(controller);
+        // ui.start();
     }
 }
